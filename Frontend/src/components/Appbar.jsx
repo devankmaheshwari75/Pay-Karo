45030d95ba0e55178c00dcf0a4198f1a548428a6
devankmaheshwari75 <maheshwaridevank@gmail.com>
2024-09-24 11:47:19 +0530

    removed button

--- Frontend/src/components/Appbar.jsx
+++ Frontend/src/components/Appbar.jsx
@@ -10,13 +10,11 @@ export const Appbar = () => {
     <div className="shadow h-14 flex justify-between">
       <div className=" flex flex-col justify-center h-full ml-4">PayKaro</div>
       <div className="flex">
-        <div className="flex flex-col justify-center h-full mr-4">Hello</div>
-        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-          <div className="flex flex-col justify-center h-full text-xl">U</div>
-        </div>
+        
+       
 
-        <div className="mt-2 ">
-          <Button
+        <div className="mt-2 pr-3 ">
+          <Button 
             label={"Logout"}
             onClick={() => {
               localStorage.removeItem("token");
